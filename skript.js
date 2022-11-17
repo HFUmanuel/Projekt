131555fb7732c21247ad5e7b7a2e4569cc45adf3
HFUmanuel <manuel.franz.egerer@hs-furtwangen.de>
2022-11-17 11:05:24 +0100

    17.11

---
+++ skript.js
@@ -0,0 +1,8 @@
+let button = document.getElementById("verlaufklick");
+button.addEventElement(click, addPElement);
+function addPElement(event){
+    let p = documentElement("p");
+    p.textContent = "1+1=2";
+    p.className = "text";
+    let div = documentgetElementbyID("verlauf")
+}
